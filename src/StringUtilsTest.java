020721d912907bef6f5cc075e1e537219ae2e6e1
zhihuanchen <mrzhihuanchen@163.com>
2017-03-26 12:14:51 +0800

    frist

---
+++ src/StringUtilsTest.java
@@ -0,0 +1,11 @@
+
+public class StringUtilsTest {
+
+	public static void main(String[] args) {
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		
+		System.out.println(Boolean.TRUE);
+	}
+}
