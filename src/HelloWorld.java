ac050ab89d4a7eca7add3f40be9f6685416d1029
zhihuanchen <mrzhihuanchen@163.com>
2017-03-26 12:28:37 +0800

    remove 1 add 4

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,9 +3,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("1");
 		System.out.println("2");
 		System.out.println("3");
+		System.out.println("4");
 		
 	}
 }
